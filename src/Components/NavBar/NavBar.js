321514a84d0d45485d4a8fda0661b67b3c9639c8
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2018-09-17 20:28:11 -0600

    Added front end skeleton

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function NavBar(props) {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
---
+++ src/Components/Tasks/Task.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Task extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Task</h1>
+            </div>
+        )
+    }
+}
+
+export default Task
