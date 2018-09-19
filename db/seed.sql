7de06263ded342fad6c0f85528814c2e1ba84b62
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2018-09-18 22:51:07 -0600

    Updated seed file

--- db/seed.sql
+++ db/seed.sql
@@ -4,7 +4,7 @@ create table users (
     email varchar(180) not null,
     last_board_viewed integer references boards(board_id),
     profile_image text not null,
-    background_image text not null default 'https://image.freepik.com/free-vector/desk-background-with-to-do-list-and-office-supplies_23-2147614795.jpg,'
+    background_image text not null default 'https://image.freepik.com/free-vector/desk-background-with-to-do-list-and-office-supplies_23-2147614795.jpg',
     auth_id text not null
 );
 
@@ -12,7 +12,8 @@ create table boards (
     board_id serial primary key,
     name varchar(180) not null,
     board_type varchar(8) not null,
-    user_id integer references users(user_id)
+    user_id integer references users(user_id),
+    board_image text not null default 'https://rajshankar.files.wordpress.com/2013/07/to-do-list1.png'
 );
 
 create table cards (
@@ -51,4 +52,10 @@ create table users_teams (
     id serial primary key,
     user_id integer references users(user_id),
     team_id integer references teams(team_id)
+);
+
+create table boards_cards (
+    id serial primary key,
+    board_id integer references boards(board_id),
+    card_id integer references cards(card_id)
 );
