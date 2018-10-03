d8e9067530b9177edf22e471a7d2e2bbc28cd741
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2018-10-03 10:30:19 -0600

    BoardsList and Board components styled

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -16,9 +16,9 @@ class Board extends Component {
             <div className="board">
                 <Link to={`/board/${board.board_id}`}>
                     <div className="board_content">
-                        <img className="board_image" src={board.board_image} alt="board"/>
-                        <h1 className="board_name">{board.board_name}</h1>
-                        <h2 className="board_name">type: {board.board_type}</h2>
+                        <img src={board.board_image} alt="board"/>
+                        <h1>{board.board_name}</h1>
+                        <h2>Type: {board.board_type}</h2>
                     </div>
                 </Link>
                 <div className="recycle_button" onClick={() => this.props.recycleBoard(board.board_id)}>Recycle</div>
